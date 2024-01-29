324c02767067f0ac0fede46b3395c02d36031a9b
kponky <ikponkedet@gmail.com>
2024-01-29 01:51:08 +0100

    js files added

---
+++ script.js
@@ -0,0 +1,4 @@
+// const startBtn = document.getElementById("start_btn");
+// const quizInform = document.getElementById("QuizInform");
+// const exitBtn = document.getElementById("exit_btn");
+// const ContBtn = document.getElementById("contd_btn")
